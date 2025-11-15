84f296ea721d198ae7f22e6720e6f29919efcdf7
Tonyn3149 <tonyn3149@gmail.com>
2025-11-15 14:53:39 -0600

    Sign up page

---
+++ app/signup.tsx
@@ -0,0 +1,26 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "./layout;
+
+export default function Signup() {
+    return (
+    <View style={styles.container}>
+        <Text style={styles.title}>Sign up</Text>
+        <Text style={styles.email}>Email</Text>
+        <TextInput 
+            placeholder="Someone@gmail.com" 
+            placeholderTextColor="#777"
+            style={styles.username}> 
+        </TextInput>
+        <Text style={styles.passwordtext}>Password</Text>
+        <TextInput 
+            placeholder="Password1234" 
+            placeholderTextColor="#777"
+            style={styles.password}
+            secureTextEntry={true}> 
+        </TextInput>
+        <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+    </View>
+    );
+}
